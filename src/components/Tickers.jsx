5a32a150ebacfd2020af4a9b4695988906afd184
Bartas <68689437+bartasd@users.noreply.github.com>
2024-02-26 22:43:33 +0200

    initial commit

--- src/components/Tickers.jsx
+++ src/components/Tickers.jsx
@@ -3,13 +3,13 @@ import style from "./Tickers.module.css";
 export default function Numbers(){
     return (
         <>
-        <div className={`${style.tickerCont} ${style.hourCont}`}>
+        <div className={`${style.tickerCont} ${style.hourCont}`} style={{ transform: `rotate(${2 * 30}deg)` }}>
             <div className={style.hour}></div>
         </div>
-        <div className={`${style.tickerCont} ${style.minuteCont}`}>
+        <div className={`${style.tickerCont} ${style.minuteCont}`} style={{ transform: `rotate(${4 * 30}deg)` }}>
             <div className={style.minute}></div>
         </div>  
-        <div className={`${style.tickerCont} ${style.secondCont}`}>
+        <div className={`${style.tickerCont} ${style.secondCont}`} style={{ transform: `rotate(${5 * 30}deg)` }}>
             <div className={style.second}></div>
         </div>
         </>
